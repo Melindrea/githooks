697a2b26178e311730427bb1550f79c21dec1d71
Marie Hogebrandt <marie@pineberry.com>
2015-03-05 12:50:40 +0100

    --amend

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 var argv = require('yargs')
     .argv,
+    readline = require('readline'),
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    }),
     gitKeys = Object.keys(process.env).filter(function (key) {
         return (key.lastIndexOf('GIT_', 0) === 0);
     }),
@@ -13,3 +18,8 @@ console.log('Args:', argv);
 console.log('CWD:', process.cwd());
 console.log('__dirname:', __dirname);
 console.log(gitObject);
+
+rl.on('line', function (cmd) {
+  console.log('STIDIN: ' + cmd);
+  rl.close();
+});
